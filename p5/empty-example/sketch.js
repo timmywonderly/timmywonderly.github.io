1008901b98bdc05ae483832ac682149ae4e8432e
Timmy Wonderly <timmywonderly@gmail.com>
2017-10-21 09:57:52 -0700

    processing changes & links

--- p5/empty-example/sketch.js
+++ p5/empty-example/sketch.js
@@ -1,8 +1,26 @@
+var cnv;
+
+function centerCanvas() {
+  var x = (windowWidth - width) / 2;
+  var y = (windowHeight - height) / 2;
+  cnv.position(x, y);
+}
+
 function setup() {
-  // put setup code here
+  cnv = createCanvas(1000, 400);
+  centerCanvas();
+  background(50, 200, 230);
+}
+
+function windowResized() {
+  centerCanvas();
 }
 
 function draw() {
-  background(204)
-  ellipse(50,50,80,80)
+  if(mouseIsPressed) {
+    fill(0,225,0);
+  } else {
+    fill(random(200),random(200),random(200));
+  }
+  ellipse(mouseX,mouseY,80,80);
 }
