3a1fc1658a0a314269625051332f5e984fd03993
Timmy Wonderly <timmywonderly@gmail.com>
2017-10-22 08:31:18 -0700

    fix

--- p5/empty-example/robot/sketch.js
+++ p5/empty-example/robot/sketch.js
@@ -28,34 +28,21 @@ function draw() {
     background(204);
 
     //neck
-    stoke(102);  //stroke is gray
-    line(266,257,266,162);  //left
-    line(276,257,276,162); //mid
-    line(286,257,286,162); //right
-
-    //antennae
-    line(276,155,246,112);  //small
-    line(276,155,306,56);   //big
-    line(276,155,324,170); //mid
-
-    //body
-    noStoke();
+    stoke(102);  
+    stroke(102);
+    line(x + 12, y - bodyHeight, x + 12, neckY);
+    line(x + 12, neckY, x - 18, neckY - 43);
+    line(x + 12, neckY, x + 42, neckY - 99);
+    line(x + 12, neckY, x + 78, neckY + 15);
+    noStroke();
     fill(102);
-    ellipse(264,337,33,33);
+    ellipse(x, y - 33, 33, 33);
     fill(0);
-    rect(219,257,90,120);
-    fill(102);
-    rect(219,274,90,6);
-
-    //head
+    rect(x - 45, y - bodyHeight, 90, bodyHeight - 33);
     fill(0);
-    ellipse(276,155,45,45);
+    ellipse(x + 12, neckY, radius, radius);
     fill(255);
-    ellipse(288,150,14,14);
+    ellipse(x + 24, neckY - 6, 14, 14);
     fill(0);
-    ellipse(288,150,3,3);
-    fill(153);
-    ellipse(263,148,5,5);
-    ellipse(296,130,4,4);
-    ellipse(305,162,3,3);
+    ellipse(x + 24, neckY - 6, 3, 3);
 }
