f7f7746987493d37e7d9d48b85b24ed49413fb41
Timmy Wonderly <timmywonderly@gmail.com>
2017-10-23 13:21:34 -0700

    .

--- p5/empty-example/sketch.js
+++ p5/empty-example/sketch.js
@@ -18,9 +18,9 @@ function windowResized() {
 
 function draw() {
   if(mouseIsPressed) {
-    fill(0,random(225),0);
+    fill(random(200));
     ellipse(mouseX,mouseY,40,80);
-    fill(0,0,random(225));
+    fill(random(200),random(200),random(200));
     triangle(mouseX,mouseY,mouseX - 50,mouseY - 40,mouseX + 45,mouseY - 50)
   } else {
     fill(random(200),random(200),random(200));
