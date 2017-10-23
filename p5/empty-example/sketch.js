ccacdafd4f207163308464283656262908f0ed16
Timmy Wonderly <timmywonderly@gmail.com>
2017-10-23 13:42:07 -0700

    .

--- p5/empty-example/sketch.js
+++ p5/empty-example/sketch.js
@@ -24,7 +24,7 @@ function draw() {
     triangle(mouseX,mouseY,mouseX - 50,mouseY - 40,mouseX + 45,mouseY - 50)
   } else {
     fill(random(200),random(200),random(200));
-    ellipse(mouseX,mouseY,40,80);
+    ellipse(mouseX,mouseY,80,40);
     fill(random(200));
     triangle(mouseX,mouseY,mouseX - 50,mouseY - 40,mouseX + 45,mouseY - 50)
     
