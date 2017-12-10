35aa026ffbd1dfb64e5742331cb275170fc3d610
Timmy Wonderly <timmywonderly@gmail.com>
2017-12-10 12:42:09 -0800

    .

--- p5/empty-example/circle/sketch.js
+++ p5/empty-example/circle/sketch.js
@@ -28,17 +28,11 @@ function draw() {
     if (mouseIsPressed) {
         radius ++;
         fill(0, 0, 144, random(50));
-        translate(mouseX, mouseY); {
-            if (radius) == 400 {
-            radius --; }
-        }
+        translate(mouseX, mouseY); 
     } else {
         radius --;
         fill(0, 138, 0, random(50);
-        translate(mouseX + 30, mouseY - 25); {
-            if (radius) == 400 {
-                radius --; }
-        }
+        translate(mouseX + 30, mouseY - 25); 
     }
     ellipse(x, y, radius, radius); 
     angle += speed;}
